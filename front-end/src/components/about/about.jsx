f0c6a38ba414d2f6d223f0f919386e547f1dcc03
Hansel <you@example.com>
2023-05-07 22:53:57 -0400

    fixed  ABOUT

---
+++ front-end/src/components/about/about.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './about.css';
+;
+
+
+function About() {
+  return (
+    <div className="about">
+    <div class="about-me-card">
+
+ <div className='hansel'>
+
+      <h2>Hansel Quiroz</h2>
+      <p>I am a software engineer with a passion for building web applications.</p>
+      <h3>My role in this project:</h3>
+      <p>Front-End</p>
+      <h3>Places I have visited:</h3>
+      <ul>
+        <li>Dominican Republic, Mexico</li>
+        <li>Jamaica, Belize</li>
+        <li>New York City, USA</li>
+      </ul>
+      <br />
+      <hr />
+  </div>
+
+
+
+    </div>
+    </div>
+    
+  );
+}
+
+export default About;
